55847fa837e19a402fa159018eabdd57df06548e
Liliya Lyubenova <lilly.lubenova@gmail.com>
2022-04-17 19:09:11 +0300

    [Update] Add logout feature

--- src/assets/pages/Dashboard/dashboardPage.js
+++ src/assets/pages/Dashboard/dashboardPage.js
@@ -2,15 +2,22 @@ import React from "react";
 import "./dashboardPage.css";
 import { appTheme } from "../../themes/appThemes.js";
 import {ThemeProvider } from "@mui/material/styles";
+import { Button } from "@mui/material";
 
-function DashboardPage() {
+function DashboardPage(props) {
   return (
     <ThemeProvider theme={appTheme}>
+      <Button onClick={() => { logoutUser(props); }}>Logout</Button>
       <div className="bg">
       <h3>TTTTTTTTTTTTTTTTT</h3>
     </div>
     </ThemeProvider>
   );
+}
+
+function logoutUser(props){
+  localStorage.removeItem("user");
+  props.history.push("/");
 }
 
 export default DashboardPage;
